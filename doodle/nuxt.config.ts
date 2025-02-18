8da30b8ee1e1d423b9f6e1363d576ab47e9758e9
Radek Zitek <radek.zitek@proton.me>
2025-02-18 21:03:21 +0100

    Add Axios dependency and update Nuxt configuration

--- doodle/nuxt.config.ts
+++ doodle/nuxt.config.ts
@@ -1,5 +1,6 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
 })
